77bc416fb4f96c13e9100e911a70763c06596311
tacostacostacos <david.epstein@libertymutual.com>
2017-11-03 11:25:08 -0400

    Add Upload model
    Upload has url and description

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -1,6 +1,8 @@
 'use strict'
+const mongoose = require('../app/middleware/mongoose')
 
 const s3Upload = require('../lib/aws-s3-upload')
+const Upload = require('../app/models/upload')
 
 const file = {
   path: process.argv[2],
@@ -8,5 +10,15 @@ const file = {
 }
 
 s3Upload(file)
-  .then((s3Response) => console.log('s3 response is ?', s3Response))
+  .then((s3Response) => {
+    console.log('s3 response is ?', s3Response.Location)
+    return Upload.create({
+      description: 'does this work?',
+      url: 'what is the url?'
+    })
+  })
+  .then(console.log)
   .catch(console.error)
+  .then(() => {
+    mongoose.connection.close()
+  })
